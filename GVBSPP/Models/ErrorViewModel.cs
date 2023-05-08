8f263907d169d6084800ed3ad8b8a8d308b5609b
Leger DJIBA <leger.djiba@outlook.fr>
2023-05-08 11:49:28 +0000

    Update : First commit

---
+++ GVBSPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GVBSPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
